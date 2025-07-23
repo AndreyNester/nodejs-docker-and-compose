1b80707f6db5583cba57628ab973e42f25d6c940
Nesterok Andrey <and-nesterok@mail.ru>
2025-07-23 07:35:03 +0400

    fix: fied .env

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -26,7 +26,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
       inject: [ConfigService],
       useFactory: (configService: ConfigService) => ({
         type: 'postgres',
-        host: 'localhost' /*configService.get('POSTGRES_HOST')*/,
+        host: configService.get('POSTGRES_HOST'),
         port: parseInt(configService.get('POSTGRES_PORT', '5432')),
         username: configService.get('POSTGRES_USER'),
         password: configService.get('POSTGRES_PASSWORD'),
