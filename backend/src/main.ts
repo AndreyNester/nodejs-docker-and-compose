cc6f90ea761f9f88a19058401c724fc41139ab04
Nesterok Andrey <and-nesterok@mail.ru>
2025-07-23 07:50:55 +0400

    fix: fixed bug

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,9 +6,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors({
-    origin: 'http://localhost:3001',
+    origin: 'http://localhost:3000',
     credentials: true,
   });
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 5000);
 }
 bootstrap();
