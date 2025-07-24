0d09ee3cf56171b52c62b1bfed5b9d689482b496
Nesterok Andrey <and-nesterok@mail.ru>
2025-07-24 08:11:00 +0400

    feat: added oririgns to cores opolicy & replaed base url at FE

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,7 +6,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: [
+      'http://localhost:3000',
+      'https://api.nesterok.kp.nomorepartiessbs.ru',
+    ],
     credentials: true,
   });
   await app.listen(process.env.PORT ?? 5000);
